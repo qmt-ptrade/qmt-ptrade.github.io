31664fbb73671e40def21e64b9f4e1b43a4f6ba9
qmt-ptrade <yagamil@hotmail.com>
2023-08-15 13:34:41 +0800

    udpate blog

---
+++ syncup.bat
@@ -0,0 +1,4 @@
+REM scp -P 22228 *.md root@117.50.175.2:/root/github/qmt-ptrade-blog/source/_posts/
+scp -P 22228 *.md root@117.50.175.2:/root/github/qmt-ptrade-blog/source/_posts/
+
+pause
---
+++ syncup.sh
@@ -0,0 +1,2 @@
+scp -P 22228 *.md root@117.50.175.2:/root/github/qmt-ptrade-blog/source/_posts/
+
